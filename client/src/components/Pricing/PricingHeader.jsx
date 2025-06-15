777b3a50f6b57c6407c309bc94abb7401bd7f5dd
osamaashraf6 <osamaashraf668@gmail.com>
2025-06-15 14:40:48 +0300

    Make the required changes

--- client/src/components/Pricing/PricingHeader.jsx
+++ client/src/components/Pricing/PricingHeader.jsx
@@ -1,42 +1,49 @@
-import {React , useState} from 'react'
+import { React, useState } from "react";
 
-import { Button } from '@/components/UI/button';
+import { Button } from "@/components/UI/button";
 
-function pricingHeader() {
-    const [billingCycle, setBillingCycle] = useState("monthly");
+function PricingHeader() {
+  const [billingCycle, setBillingCycle] = useState("monthly");
   return (
     <div>
-        <div className="container  pb-[130px]">
+      <div className="container  pb-[130px]">
         <div className="text-center">
-            <h2 className=" text-black pb-[30px] pt-[120px] text-3xl">
-                Plans & <span className="text-[#00B7C1]">Pricing</span>
-            </h2>
-            <p className="text-gray-600 pb-[5px]">Looking for advanced features? Check out our latest plans</p>
-            
+          <h2 className=" text-black pb-[30px] pt-[120px] text-3xl">
+            Plans & <span className="text-[#00B7C1]">Pricing</span>
+          </h2>
+          <p className="text-gray-600 pb-[5px]">
+            Looking for advanced features? Check out our latest plans
+          </p>
         </div>
-        <div className="flex justify-center bg-white border-grey-300 my-[30px] w-[20%] mx-auto">
-            <Button onClick={() => setBillingCycle("monthly")}
-            className={`w-[130px] text-white  capitalize h-[45px] rounded-3xl cursor-pointer
+        <div className="flex justify-center bg-white border-grey-300 my-[30px] w-[20%] mx-auto    rounded-3xl  ">
+          <Button
+            onClick={() => setBillingCycle("monthly")}
+            className={`w-[130px] text-white  rounded-3xl  shadow-none  capitalize h-[45px]  cursor-pointer
 
-                ${billingCycle === "monthly" ? 'bg-[#00B7C1] text-white' : "bg-white text-black hover:!bg-white hover:!text-black"
+                ${
+                  billingCycle === "monthly"
+                    ? "bg-[#00B7C1]   rounded-3xl      text-white"
+                    : "bg-white text-black hover:!bg-white hover:!text-black"
                 }`}
-            >
-                Monthly
-                </Button>
+          >
+            Monthly
+          </Button>
+          <Button
+            onClick={() => setBillingCycle("yearly")}
+            className={`w-[130px] text-white  shadow-none  rounded-3xl  capitalize h-[45px]    cursor-pointer
 
-                   <Button onClick={() => setBillingCycle("yearly")}
-            className={`w-[130px] text-white  capitalize h-[45px] rounded-3xl   cursor-pointer
-
-                ${billingCycle === "yearly" ? 'bg-[#00B7C1] text-white ' : "bg-white text-black hover:!bg-white hover:!text-black"
+                ${
+                  billingCycle === "yearly"
+                    ? "bg-[#00B7C1]   rounded-3xl      text-white "
+                    : "bg-white text-black hover:!bg-white hover:!text-black"
                 }`}
-            >
-                yearly
-                </Button>
-           
+          >
+            yearly
+          </Button>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default pricingHeader
+export default PricingHeader;
