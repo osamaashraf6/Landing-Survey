b43da55d28a4db86af55fc0bcb8ba5d4afba941f
osamaashraf6 <osamaashraf668@gmail.com>
2025-06-15 14:56:54 +0300

    Make the required changes

--- client/src/assets/icons/Icons.js
+++ client/src/assets/icons/Icons.js
@@ -1,7 +1,14 @@
-
 import { FaArrowRight } from "react-icons/fa6";
 import { LiaTimesSolid } from "react-icons/lia";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-export { LiaTimesSolid, HiBars3BottomRight, FaLinkedinIn, FaXTwitter,FaArrowRight };
+import { HiCheck } from "react-icons/hi";
+export {
+  LiaTimesSolid,
+  HiBars3BottomRight,
+  FaLinkedinIn,
+  FaXTwitter,
+  FaArrowRight,
+  HiCheck,
+};
--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -1,208 +1,256 @@
-import * as Img from '@/assets/images/Images';
-
+import * as Img from "@/assets/images/Images";
 
 const cardData = [
+  {
+    title: "Create Your Survey",
+    content:
+      "Choose a template or start from scratch using our easy-to-use builder",
+  },
+  {
+    title: "Share With Your Audience",
+    content:
+      "Send your survey via email, social media, or embed it in your website",
+  },
+  {
+    title: "Analyze Responses",
+    content: "Get instant insights with clean charts and downloadable reports",
+  },
+];
+const cardDetails = {
+  feedBack: [
     {
-        title: 'Create Your Survey',
-        content: 'Choose a template or start from scratch using our easy-to-use builder'
+      img: Img.feedback1,
+      title: "Customer Satisfaction",
+      content: "Evaluates satisfaction with a specific interaction or purchase",
     },
     {
-        title: 'Share With Your Audience',
-        content: 'Send your survey via email, social media, or embed it in your website'
+      img: Img.feedback2,
+      title: "Net Promoter Score (NPS)",
+      content: "Measures likelihood of customers recommending your brand",
     },
     {
-        title: 'Analyze Responses',
-        content: 'Get instant insights with clean charts and downloadable reports'
+      img: Img.feedback3,
+      title: "Service Quality Survey",
+      content: "Assesses customer service interactions",
     },
-]
-    const cardDetails = {
-        feedBack: [
-            {
-                img: Img.feedback1,
-                title: "Customer Satisfaction",
-                content: "Evaluates satisfaction with a specific interaction or purchase",
-            },
-            {
-                img: Img.feedback2,
-                title: "Net Promoter Score (NPS)",
-                content: "Measures likelihood of customers recommending your brand",
-            },
-            {
-                img: Img.feedback3,
-                title: "Service Quality Survey",
-                content: "Assesses customer service interactions",
-            },
-            {
-                img: Img.feedback4,
-                title: "Post-Purchase Feedback",
-                content: "Gathers feedback after a transaction",
-            }, {
-                img: Img.feedback1,
-                title: "Customer Satisfaction",
-                content: "Evaluates satisfaction with a specific interaction or purchase",
-            },
-            {
-                img: Img.feedback2,
-                title: "Net Promoter Score (NPS)",
-                content: "Measures likelihood of customers recommending your brand",
-            },
-            {
-                img: Img.feedback3,
-                title: "Service Quality Survey",
-                content: "Assesses customer service interactions",
-            },
-            {
-                img: Img.feedback4,
-                title: "Post-Purchase Feedback",
-                content: "Gathers feedback after a transaction",
-            },
-        ],
-        market:[
-            {
-                img: Img.market1,
-                title: "Brand Awareness Survey",
-                content: "Measures how familiar people are with your brand",
-            },
-            {
-                img: Img.market2,
-                title: "Product Pricing Survey",
-                content: "Gathers feedback on pricing acceptability",
-            },
-            {
-                img: Img.market3,
-                title: "Consumer Preferences Survey",
-                content: "Identifies customer needs and wants",
-            },
-            {
-                img: Img.market4,
-                title: "Competitor Analysis Survey",
-                content: "Compares your brand to competitors",
-            }, {
-                img: Img.market1,
-                title: "Brand Awareness Survey",
-                content: "Measures how familiar people are with your brand",
-            },
-            {
-                img: Img.market2,
-                title: "Product Pricing Survey",
-                content: "Gathers feedback on pricing acceptability",
-            },
-            {
-                img: Img.market3,
-                title: "Consumer Preferences Survey",
-                content: "Identifies customer needs and wants",
-            },
-            {
-                img: Img.market4,
-                title: "Competitor Analysis Survey",
-                content: "Compares your brand to competitors",
-            },
-        ],
-        education:[
-            {
-                img: Img.edu1,
-                title: "Course Evaluation Survey",
-                content: "Assesses course content and delivery",
-            },
-            {
-                img: Img.edu2,
-                title: "Learning Outcomes Survey",
-                content: "Assesses knowledge or skills gained",
-            },
-            {
-                img: Img.edu3,
-                title: "Student Satisfaction Survey",
-                content: "Measures overall educational experience",
-            },
-            {
-                img: Img.edu4,
-                title: "Instructor Feedback Survey",
-                content: "Evaluates teacher or trainer performance",
-            }, {
-                img: Img.edu1,
-                title: "Course Evaluation Survey",
-                content: "Assesses course content and delivery",
-            },
-            {
-                img: Img.edu2,
-                title: "Learning Outcomes Survey",
-                content: "Assesses knowledge or skills gained",
-            },
-            {
-                img: Img.edu3,
-                title: "Student Satisfaction Survey",
-                content: "Measures overall educational experience",
-            },
-            {
-                img: Img.edu4,
-                title: "Instructor Feedback Survey",
-                content: "Evaluates teacher or trainer performance",
-            },
-        ],
-        events:[
-            {
-                img: Img.event1,
-                title: "Post-Event Satisfaction Survey",
-                content: "Evaluates overall event experience",
-            },
-            {
-                img: Img.event2,
-                title: "Speaker Feedback Survey",
-                content: "Gathers opinions on presenters or speakers",
-            },
-            {
-                img: Img.event3,
-                title: "Venue Feedback Survey",
-                content: "Assesses event location and facilities",
-            },
-            {
-                img: Img.event4,
-                title: "Virtual Event Survey",
-                content: "Collects feedback on online events",
-            }, {
-                img: Img.event1,
-                title: "Post-Event Satisfaction Survey",
-                content: "Evaluates overall event experience",
-            },
-            {
-                img: Img.event2,
-                title: "Speaker Feedback Survey",
-                content: "Gathers opinions on presenters or speakers",
-            },
-            {
-                img: Img.event3,
-                title: "Venue Feedback Survey",
-                content: "Assesses event location and facilities",
-            },
-            {
-                img: Img.event4,
-                title: "Virtual Event Survey",
-                content: "Collects feedback on online events",
-            },
-        ],
-    };
+    {
+      img: Img.feedback4,
+      title: "Post-Purchase Feedback",
+      content: "Gathers feedback after a transaction",
+    },
+    {
+      img: Img.feedback1,
+      title: "Customer Satisfaction",
+      content: "Evaluates satisfaction with a specific interaction or purchase",
+    },
+    {
+      img: Img.feedback2,
+      title: "Net Promoter Score (NPS)",
+      content: "Measures likelihood of customers recommending your brand",
+    },
+    {
+      img: Img.feedback3,
+      title: "Service Quality Survey",
+      content: "Assesses customer service interactions",
+    },
+    {
+      img: Img.feedback4,
+      title: "Post-Purchase Feedback",
+      content: "Gathers feedback after a transaction",
+    },
+  ],
+  market: [
+    {
+      img: Img.market1,
+      title: "Brand Awareness Survey",
+      content: "Measures how familiar people are with your brand",
+    },
+    {
+      img: Img.market2,
+      title: "Product Pricing Survey",
+      content: "Gathers feedback on pricing acceptability",
+    },
+    {
+      img: Img.market3,
+      title: "Consumer Preferences Survey",
+      content: "Identifies customer needs and wants",
+    },
+    {
+      img: Img.market4,
+      title: "Competitor Analysis Survey",
+      content: "Compares your brand to competitors",
+    },
+    {
+      img: Img.market1,
+      title: "Brand Awareness Survey",
+      content: "Measures how familiar people are with your brand",
+    },
+    {
+      img: Img.market2,
+      title: "Product Pricing Survey",
+      content: "Gathers feedback on pricing acceptability",
+    },
+    {
+      img: Img.market3,
+      title: "Consumer Preferences Survey",
+      content: "Identifies customer needs and wants",
+    },
+    {
+      img: Img.market4,
+      title: "Competitor Analysis Survey",
+      content: "Compares your brand to competitors",
+    },
+  ],
+  education: [
+    {
+      img: Img.edu1,
+      title: "Course Evaluation Survey",
+      content: "Assesses course content and delivery",
+    },
+    {
+      img: Img.edu2,
+      title: "Learning Outcomes Survey",
+      content: "Assesses knowledge or skills gained",
+    },
+    {
+      img: Img.edu3,
+      title: "Student Satisfaction Survey",
+      content: "Measures overall educational experience",
+    },
+    {
+      img: Img.edu4,
+      title: "Instructor Feedback Survey",
+      content: "Evaluates teacher or trainer performance",
+    },
+    {
+      img: Img.edu1,
+      title: "Course Evaluation Survey",
+      content: "Assesses course content and delivery",
+    },
+    {
+      img: Img.edu2,
+      title: "Learning Outcomes Survey",
+      content: "Assesses knowledge or skills gained",
+    },
+    {
+      img: Img.edu3,
+      title: "Student Satisfaction Survey",
+      content: "Measures overall educational experience",
+    },
+    {
+      img: Img.edu4,
+      title: "Instructor Feedback Survey",
+      content: "Evaluates teacher or trainer performance",
+    },
+  ],
+  events: [
+    {
+      img: Img.event1,
+      title: "Post-Event Satisfaction Survey",
+      content: "Evaluates overall event experience",
+    },
+    {
+      img: Img.event2,
+      title: "Speaker Feedback Survey",
+      content: "Gathers opinions on presenters or speakers",
+    },
+    {
+      img: Img.event3,
+      title: "Venue Feedback Survey",
+      content: "Assesses event location and facilities",
+    },
+    {
+      img: Img.event4,
+      title: "Virtual Event Survey",
+      content: "Collects feedback on online events",
+    },
+    {
+      img: Img.event1,
+      title: "Post-Event Satisfaction Survey",
+      content: "Evaluates overall event experience",
+    },
+    {
+      img: Img.event2,
+      title: "Speaker Feedback Survey",
+      content: "Gathers opinions on presenters or speakers",
+    },
+    {
+      img: Img.event3,
+      title: "Venue Feedback Survey",
+      content: "Assesses event location and facilities",
+    },
+    {
+      img: Img.event4,
+      title: "Virtual Event Survey",
+      content: "Collects feedback on online events",
+    },
+  ],
+};
 const plan = [
-    {
-        title: "Basic",
-        price: "Free forever",
-        responses: "100",
-        buttonText: "Get Started",
-        highlight: false,
-    },
-    {
-        title: "Plus",
-        price: "$50",
-        responses: "1,000",
-        buttonText: "Choose Plus",
-        highlight: true,
-    },
-    {
-        title: "Business",
-        price: "$83",
-        responses: "10,000",
-        buttonText: "Go Business",
-        highlight: false,
-    },
-]
+  {
+    title: "Basic",
+    price: "Free forever",
+    responses: "100",
+    buttonText: "Get Started",
+    highlight: false,
+  },
+  {
+    title: "Plus",
+    price: "$50",
+    responses: "1,000",
+    buttonText: "Choose Plus",
+    highlight: true,
+  },
+  {
+    title: "Business",
+    price: "$83",
+    responses: "10,000",
+    buttonText: "Go Business",
+    highlight: false,
+  },
+];
 
-export { cardData, cardDetails, plan }
+const plans = [
+  {
+    title: "Basic",
+    description: "Create interactive forms that connect to your workflow",
+    price: 25,
+    subText: "What’s included",
+    features: [
+      "100 responses",
+      "1 user",
+      "Unlimited surveyland",
+      "unlimited questions",
+    ],
+    highlighted: false,
+  },
+  {
+    title: "Plus",
+    description:
+      "Make your forms more beautiful and on-brand and more features",
+    price: 50,
+    subText: "What’s included",
+    features: [
+      "1000 responses",
+      "3 user",
+      "Everything in Basic",
+      "Drop-off rates",
+    ],
+    highlighted: true,
+  },
+  {
+    title: "Business",
+    description: "Analyze performance and do more with your data",
+    price: 83,
+    subText: "What’s included",
+    features: [
+      "10,000 responses",
+      "5 user",
+      "Everything in Plus",
+      "Conversion tracking",
+    ],
+    highlighted: false,
+  },
+];
+
+export { cardData, cardDetails, plan, plans };
