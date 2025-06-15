57560449d555bf261d5d30737e74853f574e3e54
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-06-15 22:52:56 +0300

    Slidebar layout shadcn

--- client/src/layouts/SurveyLayout.jsx
+++ client/src/layouts/SurveyLayout.jsx
@@ -3,21 +3,28 @@ import { Outlet } from "react-router-dom";
 import Navbar from "@/components/layout/Navbar";
 import SurveyHeader from "@/components/layout/SurveyHeader";
 import SurveySidebar from "@/components/layout/SurveySidebar";
-
-const SurveyLayout = () => {
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+const SurveyLayout = ({children}) => {
   return (
     <div className="bg-[#f9f9fb]">
       <Navbar />
-      <SurveyHeader />
+      {/* <SurveyHeader /> */}
       <section className="">
-        <div className="asides  flex ">
+
+        <SidebarProvider>
+        <SurveySidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+            <Outlet />
+          </main>
+        </SidebarProvider>
+        {/* <div className="asides  flex ">
           <div className="asideleft border w-[365px]">
-            <SurveySidebar />
           </div>
           <div className="asideright border w-full">
-            <Outlet />
           </div>
-        </div>
+        </div> */}
       </section>
     </div>
   );
