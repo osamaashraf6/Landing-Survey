3b31d90fbf2b9d825e765ccdb8c4894e1315be94
osamaashraf6 <osamaashraf668@gmail.com>
2025-06-22 20:10:26 +0300

    Fix dark mode in sign in

--- client/src/components/login/AuthHeader.jsx
+++ client/src/components/login/AuthHeader.jsx
@@ -3,11 +3,11 @@ import { AuthDontHaveAccount } from "@/components";
 
 export default function AuthHeader() {
   return (
-    <header className="p-4">
+    <header className="p-4 border-b border-gray-200">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <div className="font-bold text-primary text-2xl">Survey</div>
-          <div className="font-bold text-black text-2xl">Land</div>
+          <div className="font-bold   text-2xl">Land</div>
         </div>
         <div className="sm:flex hidden items-center">
           <AuthDontHaveAccount />
