95e1649220ce60370da0191a8bb3e22dc40f8939
nesma011 <nesmaatef349@gmail.com>
2025-06-24 02:34:48 +0300

    last fixing

--- client/src/guards/AuthGuard.jsx
+++ client/src/guards/AuthGuard.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const AuthGuard = ({ children }) => {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser } = useSelector((state) => state.user|| {});
   return currentUser ? children : <Navigate to="/" replace />;
 };
 
