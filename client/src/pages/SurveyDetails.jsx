ee3e9201a4cd69296b0b6473e8b81cc2da17ba55
osamaashraf6 <osamaashraf668@gmail.com>
2025-07-14 14:41:48 +0300

    Fix the issues in survey details

--- client/src/pages/SurveyDetails.jsx
+++ client/src/pages/SurveyDetails.jsx
@@ -15,7 +15,6 @@ import usePreview from "@/hooks/previewHook";
 import { Link, useParams } from "react-router-dom";
 import { ThemeContext } from "@/context/ThemeContext";
 import { useForm } from "react-hook-form";
-import useQuestion from "@/hooks/questionHook";
 
 const SurveyDetails = () => {
   const [open, setOpen] = useState(false);
@@ -37,8 +36,7 @@ const SurveyDetails = () => {
     }
   };
 
-  const { useGetAllPreviewQuery } = usePreview();
-  const { createOneQuestionMutation } = useQuestion();
+  const { useGetAllPreviewQuery, createOneQuestionMutation } = usePreview();
   const { data: previewData, isPending } = useGetAllPreviewQuery(surveyId);
   const {
     register,
@@ -61,7 +59,7 @@ const SurveyDetails = () => {
   };
   return (
     <>
-      <section className="px-4 md:px-10 pt-3 pb-16 flex flex-col gap-5 w-full md:w-[70%]">
+      <section className="px-4 md:px-10 pt-3 pb-16 flex flex-col gap-5 w-full md:w-full xl:w-[85%]">
         {/*  */}
         <h2 className="text-xl">Add question type Text Area (Comment Box)</h2>
         <Button className="text-primary bg-white border py-5 w-[170px] flex justify-start border-[#00B7C1] hover:text-white cursor-pointer">
@@ -115,7 +113,7 @@ const SurveyDetails = () => {
           {open && (
             <form onSubmit={handleSubmit(handleAddQuestion)}>
               <div className="py-10 px-3 border-y border-gray-300 flex items-center gap-6">
-                <span className="text-sm text-gray-500">Q3:</span>
+                <span className="text-sm text-gray-500">Q{previewData?.data?.length + 1}:</span>
                 <div className="flex flex-col md:flex-row gap-2 md:gap-0 items-center justify-between w-full">
                   <input
                     type="text"
@@ -166,12 +164,14 @@ const SurveyDetails = () => {
                       />
                       <div className="flex items-center gap-2">
                         <button
+                          type="button"
                           onClick={() => addAnswerAfter(index)}
                           className="rounded-full border border-[#eff0f3] h-[30px] w-[30px] flex items-center justify-center text-[#00B7C1] hover:bg-[#00B7C1] hover:text-white"
                         >
                           +
                         </button>
                         <button
+                          type="button"
                           onClick={() => removeAnswer(index)}
                           className="rounded-full border border-[#eff0f3] h-[30px] w-[30px] flex items-center justify-center text-[#00B7C1] hover:bg-[#00B7C1] hover:text-white"
                         >
--- client/src/services/questionService.js
+++ client/src/services/questionService.js
@@ -2,8 +2,7 @@ import apiClient from "@/utils/apiClient";
 import globalService from "./globalService";
 
 export const addQuestion = async ({surveyId, question}) => {
-  console.log("surveyId:", surveyId);
-  console.log("question", question);
+ 
   const res = await apiClient.post(`${globalService.baseUrl}/questions/${surveyId}`, {
     questions: [].concat(question),
   });
